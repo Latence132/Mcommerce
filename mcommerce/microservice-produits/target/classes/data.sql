5665964b480c052af4360f9f6d3c22ded8333000
Latence132 <alexandre.chaumet@gadz.org>
2019-10-10 19:13:10 +0200

    mcommerce

---
+++ mcommerce/microservice-produits/target/classes/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'http://www.lapadd.com/photos/2751/7301/258251/377939/bougie-eternelle-permanente-longue-longue-duree-sans-coulure-design-trend-farluce-qult-packshot-solo-fond-noir.400.jpg', 22.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://www.ikea.com/PIAimages/0523335_PE643759_S5.JPG?f=s', 95.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://www.lepaturon.com/boutique/images_produits/protection-insecte-cheval-ventre-z.jpg', 360.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTepVDwMORWhFmeScbQt3IFR0PiyiZvqgmeIECv-wQwr0vE_EMb', 620.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQEmNox6PldufxHOYnXiHzZNB-dDEZ5Hs11RcUB4sHCDDFcVwOEg', 1200.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'http://www.astrosurf.com/luxorion/Physique/horloge-prague.jpg', 180.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'http://www.jardingue.fr/wp-content/uploads/2016/08/cage-abri-hamster.jpg', 210.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://images-na.ssl-images-amazon.com/images/I/71vBcg2B9zL._SX355_.jpg', 730.0);
